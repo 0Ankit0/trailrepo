a42f26dcddeda3f64c704c7dc287691a2424f00e
0ankit0 <ankitpaudel.1373@gmail.com>
2024-12-05 13:40:10 +0545

    chore: removed the student factory example

--- DesignPattern/CreationalDesignPattern/FactoryDesignPattern/Factory/IStudentFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using FactoryDesignPattern.DAL;
-
-namespace FactoryDesignPattern.Factory
-{
-    public interface IStudentFactory
-    {
-        Student CreateStudent(int? studentId, string firstName, string lastName, DateTime dateOfBirth, string gender);
-    }
-}
